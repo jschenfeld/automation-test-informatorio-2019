2171daba5cb2e07fec7cfe96ceb54c61d91f53d0
Jonatan Schenfeld <jonatan.schenfeld@globant.com>
2019-09-06 20:41:09 -0300

    added chrome en modo mobile emulation

--- grid/start_node.sh
+++ grid/start_node.sh
@@ -1,4 +1,4 @@
 #/bin/bash
 echo Selenium NODE 3.14.0
-#export "PATH=$PATH:/path/to/your/driver/binaries"
+#export "PATH=$PATH:/home/jonatan.schenfeld/projects/automation-test-informatorio-2019/grid"
 java -jar selenium-server-standalone-3.14.0.jar -role node -nodeConfig node.json
